4d938cd96675e493cadd45bae69f7cd930502a32
Advait Joshi <35962651+ajarch98@users.noreply.github.com>
2025-05-04 13:29:01 -0400

    Add advaitjoshi.book-recs to allowed CORS origins

--- book-recommender-backend/main.py
+++ book-recommender-backend/main.py
@@ -13,7 +13,8 @@ class BookList(BaseModel):
 
 origins = [
     "http://localhost:5173",
-    "https://book-recommender-web-app-foze.onrender.com"
+    "https://book-recommender-web-app-foze.onrender.com",
+    "https://book-recs.advaitjoshi.com",
 ]
 
 app = FastAPI()
